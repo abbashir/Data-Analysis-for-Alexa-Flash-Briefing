80bb62e82f6abac74df1a652069a637a082198c5
Royel <royelsoft@gmail.com>
2020-09-05 11:39:24 +0600

    doctor call added

--- json_creator.py
+++ json_creator.py
@@ -5,24 +5,43 @@ import random
 import string
 
 
-# Random number generator
+# # ----- Random number generator --------
 def generate_random(size, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
 
 current_time = str(datetime.datetime.now())
-
 data = {}
 
-data['Yesterday_Summary'] = {
+
+# data['Yesterday_Summary'] = {
+#     "uid": generate_random(8, "1234ABCD"),
+#     "updateDate": current_time,
+#     "titleText": "Yesterday Sales Summary",
+#     "mainText": "Welcome to Transcom Limited. Our, last day target was : " + s.YesterdayTarget +
+#                 " Taka" +
+#                 " and total sales was : " + s.YSales + " Taka" +
+#                 " Overall sales achievements are : " + s.Achievement + "%" +
+#                 " End of the day sales will be " + s.trend + ' Taka',
+#     "redirectionUrl": ""
+# }
+
+# # ---- Doctor call status ----------
+def d_call_status(ld_call, avg_call):
+    if (ld_call >= avg_call):
+        status = 'It is ' + str(round(ld_call / avg_call, 2)) + ' times higher than average call.'
+    else:
+        status = 'But, it is less than average call.'
+    return status
+
+
+data['DoctorCall'] = {
     "uid": generate_random(8, "1234ABCD"),
     "updateDate": current_time,
-    "titleText": "Yesterday Sales Summary",
-    "mainText": "Welcome to Transcom Limited. Our, last day target was : " + s.YesterdayTarget +
-                " Taka" +
-                " and total sales was : " + s.YSales + " Taka" +
-                " Overall sales achievements are : " + s.Achievement + "%" +
-                " Todays Sales Trends will be" +s.trend + 'Taka',
+    "titleText": "Yesterday Doctor Count",
+    "mainText": "This months total : " + str(s.thismonth_d_count) + " doctors are visited " +
+                " Where last day visited doctors are : " + str(s.lastday_d_count) +
+                " " + str(d_call_status(s.lastday_d_count, s.averaged_call)) + " ",
     "redirectionUrl": ""
 }
 
--- sql_queries.py
+++ sql_queries.py
@@ -11,67 +11,76 @@ connection1 = db.connect('DRIVER={SQL Server};'
                         'DATABASE=DCR_MREPORTING;'
                         'UID=sa;PWD=erp')
 
-YTargetQuery = """DECLARE @day int
-                    set @day=right(convert(varchar(8),DATEADD(D,0,GETDATE()-1),112),2)
-                    select sum(target)/@day as YesterdayTarget from TDCL_BranchTarget
-                    where YEARMONTH= CONVERT(varchar(6), getdate(), 112) """
-
-YSalesQuery = """select  Sum(EXTINVMISC) as  YesterdaySales from OESalesDetails
-                    where TRANSDATE = convert(varchar(8),DATEADD(D,0,GETDATE()-1),112)
-                     """
-YesterdayTarget = pd.read_sql_query(YTargetQuery, connection)
-YesterdayTarget = str(int(YesterdayTarget.YesterdayTarget))
-
-YSales = pd.read_sql_query(YSalesQuery, connection)
-YSales = str(int(YSales.YesterdaySales))
-Achievement = str(round((int(YSales)/int(YesterdayTarget))*100, 2))
-
-# print('Yesterday Target =', YesterdayTarget)
-# print('Yesterday Sales =', YSales)
-# print('Achievements = ', Achievement)
-
-# # ------------------ Todays Sales Trend -----------------
-trendq = """ DECLARE @DATE AS SMALLDATETIME = GETDATE()
-DECLARE @FIRSTDATEOFMONTH AS SMALLDATETIME = CONVERT(SMALLDATETIME, CONVERT(CHAR(4),YEAR(@DATE)) + '-' + CONVERT(CHAR(2),MONTH(@DATE)) + '-01')
-DECLARE @YESTERDAY AS SMALLDATETIME = DATEADD(d,-1,@DATE)
-DECLARE @This_month as CHAR(6)= CONVERT(VARCHAR(6), GETDATE(), 112)
-DECLARE @FIRSTDATEOFMONTH_STR AS CHAR(8)=CONVERT(VARCHAR(10), @FIRSTDATEOFMONTH , 112)
-DECLARE @YESTERDAY_STR AS CHAR(8)=CONVERT(VARCHAR(10), @YESTERDAY , 112)
-
-DECLARE @TotalDaysInMonth as Integer=(SELECT DATEDIFF(DAY, getdate(), DATEADD(MONTH, 1, getdate())))
-DECLARE @totalDaysGone as integer =(SELECT DATEPART(DD, getdate())-1)
-
-Select
-    SUM(ISNULL(DAY_END_SALE.NET_SALE,0))/@totalDaysGone as Trend FROM
-(
-SELECT
-SUM(CASE WHEN TRANSTYPE=1 THEN EXTINVMISC ELSE 0 END) AS GROSS_SALE,
-SUM(CASE  WHEN TRANSTYPE=2 OR TRANSTYPE=4 THEN EXTINVMISC ELSE 0 END) AS NET_RETURN,
-SUM(EXTINVMISC) AS NET_SALE
-FROM OESALESDETAILS
-WHERE TRANSDATE BETWEEN  @FIRSTDATEOFMONTH_STR AND @YESTERDAY_STR GROUP BY MSOTR
-) AS DAY_END_SALE
-                     """
-trend_df = pd.read_sql_query(trendq, connection)
-trend = str(int(trend_df['Trend']))
+# YTargetQuery = """DECLARE @day int
+#                     set @day=right(convert(varchar(8),DATEADD(D,0,GETDATE()-1),112),2)
+#                     select sum(target)/@day as YesterdayTarget from TDCL_BranchTarget
+#                     where YEARMONTH= CONVERT(varchar(6), getdate(), 112) """
+#
+# YSalesQuery = """select  Sum(EXTINVMISC) as  YesterdaySales from OESalesDetails
+#                     where TRANSDATE = convert(varchar(8),DATEADD(D,0,GETDATE()-1),112)
+#                      """
+# YesterdayTarget = pd.read_sql_query(YTargetQuery, connection)
+# YesterdayTarget = str(int(YesterdayTarget.YesterdayTarget))
+#
+# YSales = pd.read_sql_query(YSalesQuery, connection)
+# YSales = str(int(YSales.YesterdaySales))
+# Achievement = str(round((int(YSales)/int(YesterdayTarget))*100, 2))
+#
+# # print('Yesterday Target =', YesterdayTarget)
+# # print('Yesterday Sales =', YSales)
+# # print('Achievements = ', Achievement)
+#
+# # # ------------------ Todays Sales Trend -----------------
+# trendq = """ DECLARE @DATE AS SMALLDATETIME = GETDATE()
+# DECLARE @FIRSTDATEOFMONTH AS SMALLDATETIME = CONVERT(SMALLDATETIME, CONVERT(CHAR(4),YEAR(@DATE)) + '-' + CONVERT(CHAR(2),MONTH(@DATE)) + '-01')
+# DECLARE @YESTERDAY AS SMALLDATETIME = DATEADD(d,-1,@DATE)
+# DECLARE @This_month as CHAR(6)= CONVERT(VARCHAR(6), GETDATE(), 112)
+# DECLARE @FIRSTDATEOFMONTH_STR AS CHAR(8)=CONVERT(VARCHAR(10), @FIRSTDATEOFMONTH , 112)
+# DECLARE @YESTERDAY_STR AS CHAR(8)=CONVERT(VARCHAR(10), @YESTERDAY , 112)
+#
+# DECLARE @TotalDaysInMonth as Integer=(SELECT DATEDIFF(DAY, getdate(), DATEADD(MONTH, 1, getdate())))
+# DECLARE @totalDaysGone as integer =(SELECT DATEPART(DD, getdate())-1)
+#
+# Select
+#     SUM(ISNULL(DAY_END_SALE.NET_SALE,0))/@totalDaysGone as Trend FROM
+# (
+# SELECT
+# SUM(CASE WHEN TRANSTYPE=1 THEN EXTINVMISC ELSE 0 END) AS GROSS_SALE,
+# SUM(CASE  WHEN TRANSTYPE=2 OR TRANSTYPE=4 THEN EXTINVMISC ELSE 0 END) AS NET_RETURN,
+# SUM(EXTINVMISC) AS NET_SALE
+# FROM OESALESDETAILS
+# WHERE TRANSDATE BETWEEN  @FIRSTDATEOFMONTH_STR AND @YESTERDAY_STR GROUP BY MSOTR
+# ) AS DAY_END_SALE
+#                      """
+# trend_df = pd.read_sql_query(trendq, connection)
+# trend = str(int(trend_df['Trend']))
 # print(trend)
 
 
 
 # # Doctor call -------------------
-# doctorcall = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-#             declare @fstdate varchar(8) = CONVERT(varchar(8), dateadd(mm, -1,dateadd(dd, +1, eomonth(getdate()))),112)
-#
-#             select
-#             count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112)=@lstdate then doc_id end) as lastdaycount,
-#             count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112) between @fstdate and @lstdate
-#             then doc_id end) as thismonthcount
-#             from sm_doctor_visit
-#             """
-# doctorcalldf = pd.read_sql_query(doctorcall, connection1)
-# lastdaycount = int(doctorcalldf.lastdaycount)
-# thismonthcount = int(doctorcalldf.thismonthcount)
 
+doctorcall = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+            declare @fstdate varchar(8) = CONVERT(varchar(8), dateadd(mm, -1,dateadd(dd, +1, eomonth(getdate()))),112)
+
+            select
+            count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112)=@lstdate then doc_id end) as lastdaycount,
+            count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112) between @fstdate and @lstdate
+            then doc_id end) as thismonthcount
+            from sm_doctor_visit
+            """
+import datetime
+current_day = datetime.datetime.now()
+current_day = current_day.strftime("%d")
+
+doctorcalldf = pd.read_sql_query(doctorcall, connection1)
+lastday_d_count = int(doctorcalldf.lastdaycount)
+thismonth_d_count = int(doctorcalldf.thismonthcount)
+averaged_call = round(thismonth_d_count/int(current_day)-1)
+
+print('lastday_d_count',  lastday_d_count)
+print('thismonth_d_count', thismonth_d_count)
+print('averaged_call', averaged_call)
 
 ## NSM wise sales, target and achievements
 # nsm = """
@@ -104,48 +113,48 @@ trend = str(int(trend_df['Trend']))
 # print(nsm_ld_achiv)
 
 # # Chemist coverage
-overall_chemist = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-        select SUM(totalcust) as totalchemist, SUM(covercust) as coverchemist, 
-        (SUM(covercust)*100)/SUM(totalcust) as [percentage]
-        from 
-        (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
-        where --audtorg='pbnskf' and 
-        swactv=1
-        group by AUDTORG) as totalcust
-        left join
-        (select audtorg, count(distinct customer) as covercust from OESalesDetails
-        where transdate= @lstdate --and audtorg='pbnskf'
-        group by AUDTORG) as covcust
-        on totalcust.AUDTORG=covcust.AUDTORG """
+# overall_chemist = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+#         select SUM(totalcust) as totalchemist, SUM(covercust) as coverchemist,
+#         (SUM(covercust)*100)/SUM(totalcust) as [percentage]
+#         from
+#         (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
+#         where --audtorg='pbnskf' and
+#         swactv=1
+#         group by AUDTORG) as totalcust
+#         left join
+#         (select audtorg, count(distinct customer) as covercust from OESalesDetails
+#         where transdate= @lstdate --and audtorg='pbnskf'
+#         group by AUDTORG) as covcust
+#         on totalcust.AUDTORG=covcust.AUDTORG """
+#
+# overall_chemistdf = pd.read_sql_query(overall_chemist, connection)
+#
+# total_chemist = int(overall_chemistdf.totalchemist)
+# covered_chemist = int(overall_chemistdf.coverchemist)
+# chemist_cov_per = int(overall_chemistdf.percentage)
+#
+# print('Total Chemist = ', total_chemist)
+# print('Coverd Chemist =', covered_chemist)
+# print('Coverd Percentage=', chemist_cov_per, '%')
 
-overall_chemistdf = pd.read_sql_query(overall_chemist, connection)
-
-total_chemist = int(overall_chemistdf.totalchemist)
-covered_chemist = int(overall_chemistdf.coverchemist)
-chemist_cov_per = int(overall_chemistdf.percentage)
-
-print('Total Chemist = ', total_chemist)
-print('Coverd Chemist =', covered_chemist)
-print('Coverd Percentage=', chemist_cov_per, '%')
-
-lowest_chemist_coverage = """declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-        select top 5 branch.branch, SUM(totalcust) as totalcust, SUM(covercust) as covercust, 
-        (SUM(covercust)*100)/SUM(totalcust) as per
-        from 
-        (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
-        where --audtorg='pbnskf' and 
-        swactv=1
-        group by AUDTORG) as totalcust
-        left join
-        (select audtorg, count(distinct customer) as covercust from OESalesDetails
-        where transdate= @lstdate --and audtorg='pbnskf'
-        group by AUDTORG) as covcust
-        on totalcust.AUDTORG=covcust.AUDTORG
-        left join 
-        (select RTRIM(branch) as branch,skf from BRANCHLIST) as branch
-        on totalcust.AUDTORG=branch.skf
-        group by branch.branch
-        order by per asc
-        """
+# lowest_chemist_coverage = """declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+#         select top 5 branch.branch, SUM(totalcust) as totalcust, SUM(covercust) as covercust,
+#         (SUM(covercust)*100)/SUM(totalcust) as per
+#         from
+#         (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
+#         where --audtorg='pbnskf' and
+#         swactv=1
+#         group by AUDTORG) as totalcust
+#         left join
+#         (select audtorg, count(distinct customer) as covercust from OESalesDetails
+#         where transdate= @lstdate --and audtorg='pbnskf'
+#         group by AUDTORG) as covcust
+#         on totalcust.AUDTORG=covcust.AUDTORG
+#         left join
+#         (select RTRIM(branch) as branch,skf from BRANCHLIST) as branch
+#         on totalcust.AUDTORG=branch.skf
+#         group by branch.branch
+#         order by per asc
+#         """
 # lowest_chemist_coveragedf = pd.read_sql_query(lowest_chemist_coverage, connection)
 # print(lowest_chemist_coveragedf)
