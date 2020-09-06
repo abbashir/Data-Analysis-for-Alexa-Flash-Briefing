8aa23419e33c0232f79d331cfbb6bf26ceb98904
Royel <royelsoft@gmail.com>
2020-09-06 14:03:48 +0600

    NSM status added

--- json_creator.py
+++ json_creator.py
@@ -33,38 +33,54 @@ current_time = str(datetime.datetime.now())
 
 data = {}
 
-data['Yesterday_Summary'] = {
+# data['Yesterday_Summary'] = {
+#     "uid": generate_random(8, "1234ABCD"),
+#     "updateDate": current_time,
+#     "titleText": "Yesterday Sales Summary",
+#     "mainText": "Welcome to Transcom Limited. Our, last day target was : " + s.YesterdayTarget +
+#                 " Taka" +
+#                 " and total sales was : " + s.YSales + " Taka" +
+#                 " Overall sales achievements are : " + s.Achievement + "%" +
+#                 " End of the day sales will be " + s.trend + ' Taka',
+#     "redirectionUrl": ""
+# }
+#
+# data['DoctorCall'] = {
+#     "uid": generate_random(8, "1234ABCD"),
+#     "updateDate": current_time,
+#     "titleText": "Yesterday Doctor Count",
+#     "mainText": "This months total : " + str(s.thismonth_d_count) + " doctors are visited " +
+#                 " Where last day visited doctors are : " + str(s.lastday_d_count) +
+#                 " " + str(d_call_status(s.lastday_d_count, s.averaged_call)) + " ",
+#     "redirectionUrl": ""
+# }
+#
+# data['ChemistCoverage'] = {
+#     "uid": generate_random(8, "1234ABCD"),
+#     "updateDate": current_time,
+#     "titleText": "Chemist Status",
+#     "mainText": "We have total " + str(s.total_chemist) + " active chemist " +
+#                 " Where last day our sales force visited " + str(s.covered_chemist) + " chemist" +
+#                 str(chemist_status(s.chemist_cov_per, s.lowest_chemist_branch)),
+#     "redirectionUrl": ""
+# }
+
+
+data['NSMStatus'] = {
     "uid": generate_random(8, "1234ABCD"),
     "updateDate": current_time,
-    "titleText": "Yesterday Sales Summary",
-    "mainText": "Welcome to Transcom Limited. Our, last day target was : " + s.YesterdayTarget +
-                " Taka" +
-                " and total sales was : " + s.YSales + " Taka" +
-                " Overall sales achievements are : " + s.Achievement + "%" +
-                " End of the day sales will be " + s.trend + ' Taka',
+    "titleText": "NSM Yesterday Status",
+    "mainText": "Among  " + str(s.active_nsm) + " active NSM " +
+                ",".join(s.full_success_sales) + " are achieve their daily target completely. " +
+                ",".join(s.less_than95p_sales) + " makes less than 95% achievements. Possible reasons is " +
+                ",".join(s.less_than95p_sales_with_low_return) +
+                " makes low sales and " + ','.join(s.target_failed_1p_return) +
+                " makes low sales and more than 1% return",
     "redirectionUrl": ""
 }
 
-data['DoctorCall'] = {
-    "uid": generate_random(8, "1234ABCD"),
-    "updateDate": current_time,
-    "titleText": "Yesterday Doctor Count",
-    "mainText": "This months total : " + str(s.thismonth_d_count) + " doctors are visited " +
-                " Where last day visited doctors are : " + str(s.lastday_d_count) +
-                " " + str(d_call_status(s.lastday_d_count, s.averaged_call)) + " ",
-    "redirectionUrl": ""
-}
-
-data['ChemistCoverage'] = {
-    "uid": generate_random(8, "1234ABCD"),
-    "updateDate": current_time,
-    "titleText": "Chemist Status",
-    "mainText": "We have total " + str(s.total_chemist) + " active chemist " +
-                " Where last day our sales force visited " + str(s.covered_chemist) + " chemist" +
-                str(chemist_status(s.chemist_cov_per, s.lowest_chemist_branch))
-    ,
-    "redirectionUrl": ""
-}
 with open("data.json", "w") as f:
     json.dump(list(data.values()), f)
     print('JSON Data created.')
+
+
--- sql_queries.py
+++ sql_queries.py
@@ -11,154 +11,174 @@ connection1 = db.connect('DRIVER={SQL Server};'
                         'DATABASE=DCR_MREPORTING;'
                         'UID=sa;PWD=erp')
 
-YTargetQuery = """DECLARE @day int
-                    set @day=right(convert(varchar(8),DATEADD(D,0,GETDATE()-1),112),2)
-                    select sum(target)/@day as YesterdayTarget from TDCL_BranchTarget
-                    where YEARMONTH= CONVERT(varchar(6), getdate(), 112) """
-
-YSalesQuery = """select  Sum(EXTINVMISC) as  YesterdaySales from OESalesDetails
-                    where TRANSDATE = convert(varchar(8),DATEADD(D,0,GETDATE()-1),112)
-                     """
-YesterdayTarget = pd.read_sql_query(YTargetQuery, connection)
-YesterdayTarget = str(int(YesterdayTarget.YesterdayTarget))
-
-YSales = pd.read_sql_query(YSalesQuery, connection)
-YSales = str(int(YSales.YesterdaySales))
-Achievement = str(round((int(YSales)/int(YesterdayTarget))*100, 2))
-
-# print('Yesterday Target =', YesterdayTarget)
-# print('Yesterday Sales =', YSales)
-# print('Achievements = ', Achievement)
-
-# # ------------------ Todays Sales Trend -----------------
-trendq = """ DECLARE @DATE AS SMALLDATETIME = GETDATE()
-DECLARE @FIRSTDATEOFMONTH AS SMALLDATETIME = CONVERT(SMALLDATETIME, CONVERT(CHAR(4),YEAR(@DATE)) + '-' + CONVERT(CHAR(2),MONTH(@DATE)) + '-01')
-DECLARE @YESTERDAY AS SMALLDATETIME = DATEADD(d,-1,@DATE)
-DECLARE @This_month as CHAR(6)= CONVERT(VARCHAR(6), GETDATE(), 112)
-DECLARE @FIRSTDATEOFMONTH_STR AS CHAR(8)=CONVERT(VARCHAR(10), @FIRSTDATEOFMONTH , 112)
-DECLARE @YESTERDAY_STR AS CHAR(8)=CONVERT(VARCHAR(10), @YESTERDAY , 112)
-
-DECLARE @TotalDaysInMonth as Integer=(SELECT DATEDIFF(DAY, getdate(), DATEADD(MONTH, 1, getdate())))
-DECLARE @totalDaysGone as integer =(SELECT DATEPART(DD, getdate())-1)
-
-Select
-    SUM(ISNULL(DAY_END_SALE.NET_SALE,0))/@totalDaysGone as Trend FROM
-(
-SELECT
-SUM(CASE WHEN TRANSTYPE=1 THEN EXTINVMISC ELSE 0 END) AS GROSS_SALE,
-SUM(CASE  WHEN TRANSTYPE=2 OR TRANSTYPE=4 THEN EXTINVMISC ELSE 0 END) AS NET_RETURN,
-SUM(EXTINVMISC) AS NET_SALE
-FROM OESALESDETAILS
-WHERE TRANSDATE BETWEEN  @FIRSTDATEOFMONTH_STR AND @YESTERDAY_STR GROUP BY MSOTR
-) AS DAY_END_SALE
-                     """
-trend_df = pd.read_sql_query(trendq, connection)
-trend = str(int(trend_df['Trend']))
-# print(trend)
-
-
-
-# Doctor call -------------------
-
-doctorcall = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-            declare @fstdate varchar(8) = CONVERT(varchar(8), dateadd(mm, -1,dateadd(dd, +1, eomonth(getdate()))),112)
-
-            select
-            count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112)=@lstdate then doc_id end) as lastdaycount,
-            count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112) between @fstdate and @lstdate
-            then doc_id end) as thismonthcount
-            from sm_doctor_visit
-            """
-import datetime
-current_day = datetime.datetime.now()
-current_day = current_day.strftime("%d")
-
-doctorcalldf = pd.read_sql_query(doctorcall, connection1)
-lastday_d_count = int(doctorcalldf.lastdaycount)
-thismonth_d_count = int(doctorcalldf.thismonthcount)
-averaged_call = round(thismonth_d_count/int(current_day)-1)
-
-# print('lastday_d_count',  lastday_d_count)
-# print('thismonth_d_count', thismonth_d_count)
-# print('averaged_call', averaged_call)
-
-
-
-# Chemist coverage
-overall_chemist = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-        select SUM(totalcust) as totalchemist, SUM(covercust) as coverchemist,
-        (SUM(covercust)*100)/SUM(totalcust) as [percentage]
-        from
-        (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
-        where --audtorg='pbnskf' and
-        swactv=1
-        group by AUDTORG) as totalcust
-        left join
-        (select audtorg, count(distinct customer) as covercust from OESalesDetails
-        where transdate= @lstdate --and audtorg='pbnskf'
-        group by AUDTORG) as covcust
-        on totalcust.AUDTORG=covcust.AUDTORG """
-
-overall_chemistdf = pd.read_sql_query(overall_chemist, connection)
-
-total_chemist = int(overall_chemistdf.totalchemist)
-covered_chemist = int(overall_chemistdf.coverchemist)
-chemist_cov_per = int(overall_chemistdf.percentage)
-
-# print('Total Chemist = ', total_chemist)
-# print('Coverd Chemist =', covered_chemist)
-# print('Coverd Percentage =', chemist_cov_per, '%')
-
-lowest_chemist_coverage = """declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
-        select top 5 branch.branch, SUM(totalcust) as totalcust, SUM(covercust) as covercust,
-        (SUM(covercust)*100)/SUM(totalcust) as per
-        from
-        (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
-        where --audtorg='pbnskf' and
-        swactv=1
-        group by AUDTORG) as totalcust
-        left join
-        (select audtorg, count(distinct customer) as covercust from OESalesDetails
-        where transdate= @lstdate --and audtorg='pbnskf'
-        group by AUDTORG) as covcust
-        on totalcust.AUDTORG=covcust.AUDTORG
-        left join
-        (select RTRIM(branch) as branch,skf from BRANCHLIST) as branch
-        on totalcust.AUDTORG=branch.skf
-        group by branch.branch
-        order by per asc
-        """
-lowest_chemist_coveragedf = pd.read_sql_query(lowest_chemist_coverage, connection)
-lowest_chemist_branch = lowest_chemist_coveragedf['branch'].tolist()
+# YTargetQuery = """DECLARE @day int
+#                     set @day=right(convert(varchar(8),DATEADD(D,0,GETDATE()-1),112),2)
+#                     select sum(target)/@day as YesterdayTarget from TDCL_BranchTarget
+#                     where YEARMONTH= CONVERT(varchar(6), getdate(), 112) """
+#
+# YSalesQuery = """select  Sum(EXTINVMISC) as  YesterdaySales from OESalesDetails
+#                     where TRANSDATE = convert(varchar(8),DATEADD(D,0,GETDATE()-1),112)
+#                      """
+# YesterdayTarget = pd.read_sql_query(YTargetQuery, connection)
+# YesterdayTarget = str(int(YesterdayTarget.YesterdayTarget))
+#
+# YSales = pd.read_sql_query(YSalesQuery, connection)
+# YSales = str(int(YSales.YesterdaySales))
+# Achievement = str(round((int(YSales)/int(YesterdayTarget))*100, 2))
+#
+# # print('Yesterday Target =', YesterdayTarget)
+# # print('Yesterday Sales =', YSales)
+# # print('Achievements = ', Achievement)
+#
+# # # ------------------ Todays Sales Trend -----------------
+# trendq = """ DECLARE @DATE AS SMALLDATETIME = GETDATE()
+#             DECLARE @FIRSTDATEOFMONTH AS SMALLDATETIME = CONVERT(SMALLDATETIME, CONVERT(CHAR(4),YEAR(@DATE)) + '-' + CONVERT(CHAR(2),MONTH(@DATE)) + '-01')
+#             DECLARE @YESTERDAY AS SMALLDATETIME = DATEADD(d,-1,@DATE)
+#             DECLARE @This_month as CHAR(6)= CONVERT(VARCHAR(6), GETDATE(), 112)
+#             DECLARE @FIRSTDATEOFMONTH_STR AS CHAR(8)=CONVERT(VARCHAR(10), @FIRSTDATEOFMONTH , 112)
+#             DECLARE @YESTERDAY_STR AS CHAR(8)=CONVERT(VARCHAR(10), @YESTERDAY , 112)
+#
+#             DECLARE @TotalDaysInMonth as Integer=(SELECT DATEDIFF(DAY, getdate(), DATEADD(MONTH, 1, getdate())))
+#             DECLARE @totalDaysGone as integer =(SELECT DATEPART(DD, getdate())-1)
+#
+#             Select
+#                 SUM(ISNULL(DAY_END_SALE.NET_SALE,0))/@totalDaysGone as Trend FROM
+#             (
+#             SELECT
+#             SUM(CASE WHEN TRANSTYPE=1 THEN EXTINVMISC ELSE 0 END) AS GROSS_SALE,
+#             SUM(CASE  WHEN TRANSTYPE=2 OR TRANSTYPE=4 THEN EXTINVMISC ELSE 0 END) AS NET_RETURN,
+#             SUM(EXTINVMISC) AS NET_SALE
+#             FROM OESALESDETAILS
+#             WHERE TRANSDATE BETWEEN  @FIRSTDATEOFMONTH_STR AND @YESTERDAY_STR GROUP BY MSOTR
+#             ) AS DAY_END_SALE
+#         """
+#
+# trend_df = pd.read_sql_query(trendq, connection)
+# trend = str(int(trend_df['Trend']))
+# # print(trend)
+#
+#
+#
+# # Doctor call -------------------
+#
+# doctorcall = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+#             declare @fstdate varchar(8) = CONVERT(varchar(8), dateadd(mm, -1,dateadd(dd, +1, eomonth(getdate()))),112)
+#
+#             select
+#             count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112)=@lstdate then doc_id end) as lastdaycount,
+#             count (distinct case when CONVERT(varchar(8), DATEADD(day,0,visit_date),112) between @fstdate and @lstdate
+#             then doc_id end) as thismonthcount
+#             from sm_doctor_visit
+#             """
+#
+# import datetime
+# current_day = datetime.datetime.now()
+# current_day = current_day.strftime("%d")
+#
+# doctorcalldf = pd.read_sql_query(doctorcall, connection1)
+# lastday_d_count = int(doctorcalldf.lastdaycount)
+# thismonth_d_count = int(doctorcalldf.thismonthcount)
+# averaged_call = round(thismonth_d_count/int(current_day)-1)
+#
+# # print('lastday_d_count',  lastday_d_count)
+# # print('thismonth_d_count', thismonth_d_count)
+# # print('averaged_call', averaged_call)
+#
+#
+#
+# # Chemist coverage
+# overall_chemist = """ declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+#         select SUM(totalcust) as totalchemist, SUM(covercust) as coverchemist,
+#         (SUM(covercust)*100)/SUM(totalcust) as [percentage]
+#         from
+#         (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
+#         where --audtorg='pbnskf' and
+#         swactv=1
+#         group by AUDTORG) as totalcust
+#         left join
+#         (select audtorg, count(distinct customer) as covercust from OESalesDetails
+#         where transdate= @lstdate --and audtorg='pbnskf'
+#         group by AUDTORG) as covcust
+#         on totalcust.AUDTORG=covcust.AUDTORG """
+#
+# overall_chemistdf = pd.read_sql_query(overall_chemist, connection)
+#
+# total_chemist = int(overall_chemistdf.totalchemist)
+# covered_chemist = int(overall_chemistdf.coverchemist)
+# chemist_cov_per = int(overall_chemistdf.percentage)
+#
+# # print('Total Chemist = ', total_chemist)
+# # print('Coverd Chemist =', covered_chemist)
+# # print('Coverd Percentage =', chemist_cov_per, '%')
+#
+# lowest_chemist_coverage = """declare @lstdate varchar(8) = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112)
+#         select top 5 branch.branch, SUM(totalcust) as totalcust, SUM(covercust) as covercust,
+#         (SUM(covercust)*100)/SUM(totalcust) as per
+#         from
+#         (select audtorg,count(distinct idcust) as totalcust  from CustomerInformation
+#         where --audtorg='pbnskf' and
+#         swactv=1
+#         group by AUDTORG) as totalcust
+#         left join
+#         (select audtorg, count(distinct customer) as covercust from OESalesDetails
+#         where transdate= @lstdate --and audtorg='pbnskf'
+#         group by AUDTORG) as covcust
+#         on totalcust.AUDTORG=covcust.AUDTORG
+#         left join
+#         (select RTRIM(branch) as branch,skf from BRANCHLIST) as branch
+#         on totalcust.AUDTORG=branch.skf
+#         group by branch.branch
+#         order by per asc
+#         """
+# lowest_chemist_coveragedf = pd.read_sql_query(lowest_chemist_coverage, connection)
+# lowest_chemist_branch = lowest_chemist_coveragedf['branch'].tolist()
 # print(lowest_chemist_branch)
 
 
 ## NSM wise sales, target and achievements
-# nsm = """
-#         declare @totaldays varchar(8) =  DateDiff(Day,getdate(),DateAdd(month,1,getdate()))
-#         select nsmname, SUM(daywisetarget) as daywisetarget, sum(lstdaysales) as lastdaysales,
-#         --(sum(lstdaysales)*100)/SUM(daywisetarget) as per
-#         case when SUM(daywisetarget) = 0 then 0 else (sum(lstdaysales)*100)/SUM(daywisetarget) end as PercentVal
-#         from
-#         (select nsmname, msotr, SUM(target)/@totaldays as daywisetarget from RFieldForce
-#         where yearmonth= CONVERT(varchar(6), DATEADD(day,0,getdate()),112)
-#         group by NSMNAME, msotr) as nsm
-#         left join
-#         (select MSOTR, SUM(EXTINVMISC) as lstdaysales from oesalesdetails
-#         where transdate = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112) and transtype=1
-#         group by MSOTR) as sales
-#         on nsm.msotr=sales.msotr
-#         group by nsmname
-#         order by  PercentVal desc
-#                 """
-#
-# nsmdf = pd.read_sql_query(nsm, connection)
-# nsm_name = nsmdf.nsmname.tolist()
-# nsm_ld_target = nsmdf.daywisetarget.tolist()
-# nsm_ld_sales = nsmdf.lastdaysales.tolist()
-# nsm_ld_achiv = nsmdf.PercentVal.tolist()
-#
-# print(nsm_name)
-# print(nsm_ld_target)
-# print(nsm_ld_sales)
-# print(nsm_ld_achiv)
+nsm = """
+        declare @totaldays varchar(8) = DateDiff(Day,getdate(),DateAdd(month,1,getdate()))
+        select nsmname, SUM(daywisetarget) as daywisetarget, sum(lstdaysales) as ldsales,
+        case when SUM(daywisetarget) = 0 then 0 else (sum(lstdaysales)*100)/SUM(daywisetarget) end as PercentVal,
+        case when SUM(lstdaysales) = 0 then 0 else (sum(salesReturn.ReturnVal)*-1*100)/SUM(lstdaysales) end as ReturnPercentVal
+        
+        from
+        (select nsmname, msotr, SUM(target)/@totaldays as daywisetarget from RFieldForce
+        where yearmonth= CONVERT(varchar(6), DATEADD(day,0,getdate()),112)
+        group by NSMNAME, msotr) as nsm
+        
+        left join
+        (select MSOTR, SUM(EXTINVMISC) as lstdaysales from oesalesdetails
+        where transdate = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112) and transtype=1
+        group by MSOTR) as sales
+        on nsm.msotr=sales.msotr
+        
+        left join 
+        (select MSOTR, SUM(EXTINVMISC) as ReturnVal from oesalesdetails
+        where transdate = CONVERT(varchar(8), DATEADD(day,-1,getdate()),112) and transtype=2
+        group by MSOTR) as salesReturn
+        on nsm.msotr=salesReturn.msotr
+        group by nsmname
+        order by  PercentVal desc, ReturnPercentVal desc
+                """
+
+nsmdf = pd.read_sql_query(nsm, connection)
+
+actual_nsm = nsmdf[nsmdf.daywisetarget > 1]
+active_nsm = actual_nsm.daywisetarget.count()
+
+full_success_sales = actual_nsm[actual_nsm.PercentVal >= 100]
+full_success_sales = full_success_sales.nsmname.tolist()
+print('Hundred percent success = ', full_success_sales)
+
+less_than95p_sales = actual_nsm[actual_nsm.PercentVal < 95]
+less_than95p_sales = less_than95p_sales.nsmname.tolist()
+print('Less than 95% sales = ', less_than95p_sales)
+
+less_than95p_sales_with_low_return = actual_nsm[(actual_nsm['PercentVal'] <= 95) & (actual_nsm['ReturnPercentVal'] < 1)]
+less_than95p_sales_with_low_return = less_than95p_sales_with_low_return.nsmname.tolist()
+print('Less than 95% sales with les 1% return = ', less_than95p_sales_with_low_return)
+
+target_failed_1p_return = actual_nsm[(actual_nsm['PercentVal'] <= 95) & (actual_nsm['ReturnPercentVal'] >= 1)]
+target_failed_1p_return = target_failed_1p_return.nsmname.tolist()
+print('Make more than 1 % returns = ', target_failed_1p_return)
